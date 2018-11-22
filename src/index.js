cf498e6c76a0718712fbb53401102d5811f023e6
Bart Borowiec <bartekborowiec91@gmail.com>
2018-11-22 23:35:58 +0100

    fixed background file path

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-import './images/background.png';
+import './images/background.jpg';
 import './index.css';
 
 import App from './components/App';
